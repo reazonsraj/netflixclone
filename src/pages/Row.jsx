98156b7d245cf6ede6ed003e06cde6e45e7041d6
Raj Tuladhar <reazonsraj@gmail.com>
2022-08-17 01:05:53 +0930

    without the slider

--- src/pages/Row.jsx
+++ src/pages/Row.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
+
+
 function Row({title, fetchURL}) {
   return (
     <div>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+        <div className='relaive flex items-center'>
+            <div >
+
+            </div>
+
+        </div>
     </div>
   )
 }
 
-export default Row
+export default Row;
