008bfed8da3a44c1b4bb1a0abe2e05f104a6e3e2
Raj Tuladhar <reazonsraj@gmail.com>
2022-08-18 01:03:21 +0930

    modified view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
   return (
     <div>
 <Main/>
-<Row  rowID='1' title ='Up Comining' fetchURL={requests.requestUpcoming}/>
+<Row  rowID='1'  title ='Up Comining New' fetchURL={requests.requestUpcoming} />
 <Row  rowID='2'title ='Popular' fetchURL={requests.requestPopular}/>
 <Row  rowID='3'title ='Trending' fetchURL={requests.requestTrending}/>
 <Row rowID='4' title ='Top Rated' fetchURL={requests.requestTopRated}/>
--- src/pages/Row.jsx
+++ src/pages/Row.jsx
@@ -28,12 +28,12 @@ const slideRight=()=>{
 }
   return (
     <div>
-        <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+        <h1 className='text-white font-bold md:text-xl lg:text-[40px] p-4'>{title}</h1>
         <div className='relaive flex items-center group'>
           <MdChevronLeft onClick={slideLeft} className='bg-white rounded-full absolute-opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
             <div id={'slider' + rowID} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                 {movies.map((item,id)=>(
-                  <div className='w-[160px] sm:w-[200px] md:w-[2400px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                  <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[550px] inline-block cursor-pointer relative p-2'>
                     <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
                     <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                   <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
