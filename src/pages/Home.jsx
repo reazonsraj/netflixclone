f48734c7725af2c8a1542be19e90e784058e8c3c
Raj Tuladhar <reazonsraj@gmail.com>
2022-08-17 23:54:26 +0930

    clone with UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,11 @@ export const Home = () => {
   return (
     <div>
 <Main/>
-<Row title ='Up Comining' fetchURL={requests.requestUpcoming}/>
-<Row title ='Popular' fetchURL={requests.requestPopular}/>
-<Row title ='Trending' fetchURL={requests.requestTrending}/>
-<Row title ='Top Rated' fetchURL={requests.requestTopRated}/>
-<Row title ='Horror' fetchURL={requests.requestHorror}/>
+<Row  rowID='1' title ='Up Comining' fetchURL={requests.requestUpcoming}/>
+<Row  rowID='2'title ='Popular' fetchURL={requests.requestPopular}/>
+<Row  rowID='3'title ='Trending' fetchURL={requests.requestTrending}/>
+<Row rowID='4' title ='Top Rated' fetchURL={requests.requestTopRated}/>
+<Row  rowID='5'title ='Horror' fetchURL={requests.requestHorror}/>
     </div>
   )
 }
